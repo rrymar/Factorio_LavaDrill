e0317c6212b7aa5cdac12d01681671538053642d
rrymar <rrymar@outlook.com>
2025-07-20 14:35:45 +0300

    recipe disable

--- prototypes/lava-drill/recipe.lua
+++ prototypes/lava-drill/recipe.lua
@@ -2,13 +2,13 @@ data:extend({
     {
         type = "recipe",
         name = "lava-drill",
-        enabled = true,
+        enabled = false,
         ingredients =
         {
-            { type = "item", name = "steel-plate", amount = 1000 },
-            { type = "item", name = "pipe", amount = 1000 },
-            { type = "item", name = "advanced-circuit", amount = 1000 },
-            { type = "item", name = "iron-gear-wheel", amount = 1000 }
+            { type = "item", name = "steel-plate", amount = 100 },
+            { type = "item", name = "pipe", amount = 200 },
+            { type = "item", name = "advanced-circuit", amount = 50 },
+            { type = "item", name = "iron-gear-wheel", amount = 200 }
         },
         results = { { type = "item", name = "lava-drill", amount = 1 } }
     }
